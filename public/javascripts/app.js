5ceecfa68b2744015e4fff0e6b4b093a9974f5d5
Conor Sloan <conor@Conors-MacBook-Pro.local>
2014-08-24 14:18:12 +0100

    Basic UI for pages and page title logic

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,11 +12,18 @@ angular.module('portfolioSite', [
     'portfolioSite.controllers'
 ]).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
-        $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutController'});
-        $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'ProjectsController'});
-        $routeProvider.when('/projects/:projectName', {templateUrl: 'partials/project.html', controller: 'ProjectController'});
-        $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'ContactController'});
-        $routeProvider.when('/cv', {templateUrl: 'partials/cv.html', controller: 'CvController'});
+        $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController', title: 'Home'});
+        $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutController', title: 'About Me'});
+        $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'ProjectsController', title: 'Projects'});
+        $routeProvider.when('/projects/:projectName', {templateUrl: 'partials/project.html', controller: 'ProjectController', title: 'Project'});
+        $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'ContactController', title: 'Contact Me'});
+        $routeProvider.when('/cv', {templateUrl: 'partials/cv.html', controller: 'CvController', title: 'CV'});
         $routeProvider.otherwise({redirectTo: '/home'});
-    }]);
+    }])
+
+    .run(function($rootScope) {
+        $rootScope.$on("$routeChangeSuccess", function(event, currentRoute, previousRoute) {
+            $rootScope.title = currentRoute.title;
+        });
+    });
+
