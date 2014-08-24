91e9a41bd96ea176012c7c20c7ab815aff8ead64
Conor Sloan <conor@conors-mbp.home>
2014-08-24 23:10:26 +0100

    openshift

--- bin/server.js
+++ bin/server.js
@@ -2,12 +2,13 @@
  * Created by conor on 24/08/2014.
  */
 
-var debug = require('debug')('portfolioSite');
 var app = require('../app');
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ipaddr', server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+var ipaddr = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-});
+app.set('port', port);
+app.set('ipaddr', ipaddr);
+
+var server = app.listen(app.get('port'), app.get('ipaddr'));
+
