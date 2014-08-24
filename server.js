b3865e54acc59157e2a4ecbeeec81083a01ff058
Conor Sloan <conor@conors-mbp.home>
2014-08-24 23:34:22 +0100

    openshift

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var app = require('./app');
 var ipaddr = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
+console.log('PORT: '+port+' IP ADDR: '+ipaddr);
+
 app.set('port', port);
 app.set('ipaddr', ipaddr);
 
 var server = app.listen(app.get('port'), app.get('ipaddr'));
-
+console.log('Listening...');
