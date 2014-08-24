fecc909139a3d414032d1539c098673833f84fa7
Conor Sloan <conor@conors-mbp.home>
2014-08-24 22:53:19 +0100

    Openshift setup

---
+++ bin/server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by conor on 24/08/2014.
+ */
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
