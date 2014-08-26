3a00af82cb8ba8adc702fd7419d3afe91e74d1ca
Conor Sloan <c.sloan7597@googlemail.com>
2014-08-26 20:16:20 +0100

    Use bower for client-side dependencies

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // Database setup
 //var databaseUrl = 'portfolioSiteDB';
