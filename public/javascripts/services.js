555871c5e49957f60ebaaacd25118596fdf21fb1
Conor Sloan <c.sloan7597@googlemail.com>
2015-03-01 17:07:09 +0000

    http service caching

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -10,15 +10,15 @@ services.factory('projectService', ['$http', function ($http) {
 
     return {
         getProjectInfo: function (callback) {
-            $http.get('/api/projectinfo').success(callback);
+            $http.get('/api/projectinfo', {cache: true}).success(callback);
         },
 
         getAllProjects: function (callback) {
-            $http.get('/api/projects').success(callback);
+            $http.get('/api/projects', {cache: true}).success(callback);
         },
 
         getProject: function (name, callback) {
-            $http.get('/api/project/' + name).success(callback);
+            $http.get('/api/project/' + name, {cache: true}).success(callback);
         }
     };
 }]);
@@ -29,7 +29,7 @@ services.factory('aboutService', ['$http', function ($http) {
 
     return {
         getAboutMe: function (callback) {
-            $http.get('/api/about').success(callback);
+            $http.get('/api/about', {cache: true}).success(callback);
         }
     };
 }]);
@@ -40,15 +40,15 @@ services.factory('cvService', ['$http', function ($http) {
 
     return {
         getJobs: function (callback) {
-            $http.get('/api/cv/jobs').success(callback);
+            $http.get('/api/cv/jobs', {cache: true}).success(callback);
         },
 
         getTechUsed : function (callback) {
-            $http.get('/api/cv/tech').success(callback);
+            $http.get('/api/cv/tech', {cache: true}).success(callback);
         },
 
         getAtAGlanceContent: function (callback) {
-            $http.get('/api/cv/atAGlanceContent').success(callback);
+            $http.get('/api/cv/atAGlanceContent', {cache: true}).success(callback);
         }
     };
 }]);
